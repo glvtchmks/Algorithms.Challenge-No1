2de30727be31fe50aa1b396f3026b7f4d02213eb
glvtchmks <126572561+glvtchmks@users.noreply.github.com>
2023-02-28 14:36:54 +0200

    Update Algorithms.Challenge_No1.py

--- Algorithms.Challenge No1/Algorithms.Challenge_No1.py
+++ Algorithms.Challenge No1/Algorithms.Challenge_No1.py
@@ -1,12 +1,12 @@
 import os
 
-input_file_path = input("Введіть шлях до вхідного файлу: ")
+input_file_path = input("Введiть шлях до вхiдного файлу: ")
 
 if not os.path.exists(input_file_path):
     print("Файл не знайдено!")
     exit()
 
-output_file_path = input("Введіть шлях до вихідного файлу: ")
+output_file_path = input("Введiть шлях до вихiдного файлу: ")
 
 with open(input_file_path, "r", encoding="utf-8") as input_file:
     data = input_file.read()
@@ -16,4 +16,4 @@ with open(input_file_path, "r", encoding="utf-8") as input_file:
     with open(output_file_path, "w", encoding="utf-8") as output_file:
         output_file.write(reversed_data)
         
-    print("Дані успішно записані у файл.")
+    print("Данi успiшно записанi у файл.")
